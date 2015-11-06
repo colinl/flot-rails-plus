0bda4216111630ef0d4ca08c6a632bdddafa3972
colinl <colin@clanlaw.org.uk>
2015-11-06 12:29:09 +0000

    Fix test helpers (json parse errors in some combinations of ruby/gems)

--- lib/flot/rails/plus/test_helper.rb
+++ lib/flot/rails/plus/test_helper.rb
@@ -6,7 +6,7 @@ class ActiveSupport::TestCase
     chart_sets = nil
     assert_select "script", /gon\.chart_sets=/ do |element|
       # get the tag as a string
-      string = element.to_s
+      string = element[0].to_s
       # select the gon.chart_sets string as $1
       string =~ /gon\.chart_sets=(.*\}\]);/
       # parse it as json
@@ -38,7 +38,7 @@ class ActiveSupport::TestCase
     url = nil
     assert_select "script", /gon\.url=/ do |element|
       # get the tag as a string
-      string = element.to_s
+      string = element[0].to_s
       # select the gon.url string as $1
       string =~ /gon\.url=\"(.*?)\"/
       url = $1
@@ -51,7 +51,7 @@ class ActiveSupport::TestCase
     refresh = nil
     assert_select "script", /gon\.refresh=/ do |element|
       # get the tag as a string
-      string = element.to_s
+      string = element[0].to_s
       # select the gon.refresh string as $1
       string =~ /gon\.refresh=(.*?);/
       refresh = $1
